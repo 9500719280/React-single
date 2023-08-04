6beb52e751ba6867ae72e3ee5d7759680758842b
Praveenraj M <114849281+9500719280@users.noreply.github.com>
2023-08-04 15:35:43 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,146 @@
-import logo from './logo.svg';
+
+
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <div className="row">
+                <div className="a">
+                    <div className="b">
+                       <div className="d1">HOME</div>
+                       <div className="d2">ABOUT US</div>
+                       <div className="d3">PRODUCTS</div>
+                       <div className="d4">SERVICES</div>
+                       <div className="d5">CONTACT</div>
+                    </div><br/>
+                    <div className="c">
+                        <div className="d6">I</div>
+                        <div className="d7"> space</div>
+                    </div><br/>
+                    <div className="d8"> <img src="01.png" alt='4' width="100%"/></div>
+
+                    <div className="d9">
+                        <div className="d10">
+                            <div className="d11">
+                                <div>
+                                    <h1>LOREM IPSUM IS</h1>
+                                </div>
+                                <div>
+                                    <img src="02.png" alt='3' width="100%"/>
+                                </div>
+                                <div className="p"> 
+                                    <p>Lorem ipsum dolor sit amet, 
+                                    consectetur adipisicing elit,
+                                    sed do elusmod tempor incididunt 
+                                    labore dolore magna aliqua. 
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="d11">
+                                <div>
+                                    <h1>LOREM IPSUM IS</h1>
+                                </div>
+                                <div> 
+                                    <img src="03.png"  alt='2'width="100%"/>
+                                </div>
+                                <div className="p"> 
+                                    <p>Lorem ipsum dolor sit amet, 
+                                    consectetur adipisicing elit,
+                                    sed do elusmod tempor incididunt 
+                                    labore dolore magna aliqua. 
+                                    </p>
+                                </div>
+                            </div>
+                            <div className="d11">
+                                <div>
+                                    <h1>LOREM IPSUM IS</h1>
+                                </div>
+                                <div> 
+                                    <img src="04.png" alt='1'width="100%"/>
+                                </div>
+                                <div className="p"> 
+                                    <p >Lorem ipsum dolor sit amet, 
+                                    consectetur adipisicing elit,
+                                    sed do elusmod tempor incididunt 
+                                    labore dolore magna aliqua. 
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="d12">
+                        <div className="d13">
+                            <h2>Lorem Ipsum Simply</h2>
+                            <ul>
+                                <li> The standard chunk of lore ipsum used</li>
+                                <li> Dus a augue ac libero euismod viverra sitth</li>
+                                <li> The standard chunk of lore ipsum used</li>
+                                <li> Dus a augue ac libero euismod viverra sitth</li>
+                                <li> The standard chunk of lore ipsum used</li>
+                                <li> Dus a augue ac libero euismod viverra sitth</li>
+                            </ul>
+                        </div>
+                        <div className="d13">
+                            <h2>Lorem Ipsum Simply</h2>
+                            <p className="p1">
+                                The standard chunk of lorem ipsum dolor sit amet.
+                            </p>
+                            <p>There are many variations of passages of lorem 
+                               ipsum available,but the majority have suffered 
+                               alteration in some form,by injected humour,or 
+                               randomised words which don't look even slightly believable. 
+                                if you are going to use a passage.
+                            </p>
+                            <div className="read">Read More</div>
+                        </div>
+                        <div className="d13">
+                            <h2>Lorem Ipsum Simply</h2>
+                            <p className="p1">
+                                The standard chunk of lorem ipsum dolor sit amet.
+                            </p>
+                            <p>There are many variations of passages of lorem 
+                                ipsum available,but the majority have suffered 
+                                alteration in some form,by injected humour,or 
+                                randomised words which don't look even slightly believable. 
+                                if you are going to use a passage.
+                            </p>
+                            <div className="read">Read More</div>
+                        </div>
+                    </div>
+                    <div className="d14">
+                        <div className="d15">
+                            <div className="d16">
+                                <h1>15</h1>
+                            </div>
+                            <div className="d17">
+                                <h2>LOREM IPSUM IS SIMPLY TEXT</h2>
+                                <p> Lorem ipsum dolor sit amet, 
+                                    consectetur adipisicing elit,
+                                    sed do elusmod tempor incididunt 
+                                    labore dolore magna aliqua.
+                                </p>
+                            </div>
+                        </div>                     
+                        <div className="d15">
+                            <div className="d16">
+                                <h1>$</h1>
+                            </div>
+                            <div className="d17">
+                                <h2>LOREM IPSUM IS SIMPLY TEXT</h2>
+                                <p>Lorem ipsum dolor sit amet, 
+                                    consectetur adipisicing elit,
+                                    sed do elusmod tempor incididunt 
+                                    labore dolore magna aliqua.
+                                </p>
+                            </div>
+                        </div>                   
+                    </div><br/><br/>
+                    <div className="d18">DESIGN BY PRAVEENRAJ</div>
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default App;
